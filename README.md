c357bb8ed35b8ff1427008368d94645b3b694b50
Youssef Belguith <youssef.belguith@nonceblox.com>
2023-02-20 02:54:01 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# NFT-Explorer
+# NFT Explorer Quick App
+
+Clone the repository using git:
+
+```bash
+git clone 
+```
+
+Once the repository is cloned, cd into the repository directory:
+
+```bash
+cd NFT-Explorer
+```
+
+Install the requried dependencies using [Npm]:
+
+```bash
+npm install
+```
+
+Once the packages are installed, start the application by running:
+
+```bash
+npm start
+```
---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+import SearchComponent from './components/SearchComponent'
+import NFTView from './components/NFTView'
+
+function App() {
+  const [wallet, setWallet] = useState(null)
+
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      {
+        wallet ? (
+          <NFTView wallet={wallet} />
+        ) : (
+          <SearchComponent wallet={wallet} setWallet={setWallet}/>
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
